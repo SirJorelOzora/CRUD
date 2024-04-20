b0e58c197fd58d6d1867c41b87a5132541574273
JorelKarbulator <147239296+SirJorelOzora@users.noreply.github.com>
2024-04-20 14:36:23 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,8 @@
+<div class="card">
+        <div class="card-body">
+        <p>
+        <img src="gambar/WhatsApp Image 2024-03-02 at 14.18.45_6b0ad2c9.jpg" class="rounded float-end" alt="gambar" height="300px">
+            <b>ProWeb merupakan singkatan dari "Professional Website". Secara umum, ProWeb merujuk pada sebuah situs web yang dibuat dengan tujuan profesional, seringkali untuk kepentingan bisnis, organisasi, atau individu yang ingin membangun kehadiran online yang kuat dan efektif. Situs web yang dikategorikan sebagai ProWeb cenderung memiliki desain yang profesional, fungsionalitas yang kuat, dan konten yang relevan dengan target audiensnya. Dibandingkan dengan situs web yang dibuat secara informal atau hanya sebagai hobi, ProWeb biasanya memerlukan perencanaan yang lebih serius, pengembangan yang lebih cermat, dan seringkali melibatkan investasi keuangan yang lebih besar.
+            </b></p>
+        </div>
+    </div>
